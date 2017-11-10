68417d72e6bcc6140f287456c86a68d6ff3fb6c1
Praveen Kumar H <praveenkumar.h2015@vit.ac.in>
2017-11-10 16:13:20 +0530

    Backstretch Plugin
    Backstretch Plugin with Responsive Web Design.

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(e) {
+	$('.with-hover-text, .regular-link').click(function(e){
+		e.stopPropagation();
+	});
+	
+
+	$('.with-hover-text').hover(
+		function(e) {
+			$(this).css('overflow', 'visible');
+			$(this).find('.hover-text')
+				.show()
+				.css('opacity', 0)
+				.delay(200)
+				.animate(
+					{
+						paddingTop: '25px',
+						opacity: 1
+					},
+					'fast',
+					'linear'
+				);
+		},
+		function(e) {
+			var obj = $(this);
+			$(this).find('.hover-text')
+				.animate(
+					{
+						paddingTop: '0',
+						opacity: 0
+					},
+					'fast',
+					'linear',
+					function() {
+						$(this).hide();
+						$( obj ).css('overflow', 'hidden');
+					}
+				);
+		}
+	);
+	
+	var img_loaded = 0;
+	var j_images = [];
+	
+});
